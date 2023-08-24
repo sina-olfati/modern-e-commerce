461fa9aa03cdf4c559c95fc43cc904d41b3bbb0f
sina <sinaolfati5@gmail.com>
2023-08-24 12:33:33 +0330

    home page mQueries

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,6 @@ import Main1 from "../img/header/home-img-1.jpg";
 import Main2 from "../img/header/home-img-2.jpg";
 import Main3 from "../img/header/home-img-3.jpg";
 import Main4 from "../img/header/home-img-4.jpg";
-import { Link } from "react-router-dom";
 
 function Hero() {
   return (
@@ -12,32 +11,24 @@ function Hero() {
         <div className="container">
           <div className="grid-container">
             <div className="featured grid-one">
-              <Link to="categories/furnitures">
-                <div id="img1" className="lil-overlay"></div>
-                <img src={Main1} alt="img1" />
-                <p className="main-description">Live Comfortably</p>
-              </Link>
+              <div id="img1" className="lil-overlay"></div>
+              <img src={Main1} alt="img1" />
+              <p className="main-description">Live Comfortably</p>
             </div>
             <div className="featured grid-two">
-              <Link to="categories/skin-care">
-                <div id="img2" className="lil-overlay"></div>
-                <img src={Main2} alt="img2" />
-                <p className="main-description">Skincare</p>
-              </Link>
+              <div id="img2" className="lil-overlay"></div>
+              <img src={Main2} alt="img2" />
+              <p className="main-description">Skincare</p>
             </div>
             <div className="featured grid-four">
-              <Link to="categories/kitchen">
-                <div id="img3" className="lil-overlay"></div>
-                <img src={Main3} alt="img3" />
-                <p className="main-description">Kitchen</p>
-              </Link>
+              <div id="img3" className="lil-overlay"></div>
+              <img src={Main3} alt="img3" />
+              <p className="main-description">Kitchen</p>
             </div>
             <div className="featured grid-four-low">
-              <Link to="categories/electronics">
-                <div id="img4" className="lil-overlay"></div>
-                <img src={Main4} alt="img4" />
-                <p className="main-description">Electronics</p>
-              </Link>
+              <div id="img4" className="lil-overlay"></div>
+              <img src={Main4} alt="img4" />
+              <p className="main-description">Electronics</p>
             </div>
           </div>
         </div>
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,12 +1,7 @@
-import "../components/ProudProducts.css";
-import CategoriesHeader from "../components/CategoriesHeader";
-import { Outlet } from "react-router";
-
 function Categories() {
   return (
     <>
-      <CategoriesHeader />
-      <Outlet />
+      <h1>categories</h1>
     </>
   );
 }
