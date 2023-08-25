112ef23794a6c052b14fe2b924a473e1780e83fd
sina <sinaolfati5@gmail.com>
2023-08-25 14:48:10 +0330

    small changes to make it ready for showing

--- src/components/TrendingSlider.jsx
+++ src/components/TrendingSlider.jsx
@@ -1,12 +1,4 @@
 import "./TrendingSlider.css";
-import Img2 from "../img/products/2.jpg";
-import Img1 from "../img/products/1.jpg";
-import Img3 from "../img/products/3.png";
-import Img4 from "../img/products/4.jpg";
-import Img5 from "../img/products/5.jpg";
-import Img6 from "../img/products/6.jpg";
-import Img7 from "../img/products/7.jpg";
-import Img8 from "../img/products/8.jpg";
 import TrendingItem from "./TrendingItem";
 
 function TrendingSlider() {
@@ -26,55 +18,16 @@ function TrendingSlider() {
         <div className="title-btns">
           <h3>Trending Now</h3>
           <div className="btns">
-            <button onClick={slideLeft}>
+            <button title="scroll left" onClick={slideLeft}>
               <i className="fa-solid fa-arrow-left"></i>
             </button>
-            <button onClick={slideRight}>
+            <button title="scroll right" onClick={slideRight}>
               <i className="fa-solid fa-arrow-right"></i>
             </button>
           </div>
         </div>
         <div className="row-container" id="slider">
-          <TrendingItem
-            img={Img2}
-            description="Pop-Up Toaster, White, 45cm"
-            price="49$"
-          />
-          <TrendingItem
-            img={Img7}
-            description="Pop-Up Toaster, White, 45cm"
-            price="49$"
-          />
-          <TrendingItem
-            img={Img3}
-            description="Pop-Up Toaster, White, 45cm"
-            price="49$"
-          />
-          <TrendingItem
-            img={Img4}
-            description="Pop-Up Toaster, White, 45cm"
-            price="49$"
-          />
-          <TrendingItem
-            img={Img5}
-            description="Pop-Up Toaster, White, 45cm"
-            price="49$"
-          />
-          <TrendingItem
-            img={Img6}
-            description="Pop-Up Toaster, White, 45cm"
-            price="49$"
-          />
-          <TrendingItem
-            img={Img1}
-            description="Pop-Up Toaster, White, 45cm"
-            price="49$"
-          />
-          <TrendingItem
-            img={Img8}
-            description="Pop-Up Toaster, White, 45cm"
-            price="49$"
-          />
+          <TrendingItem />
         </div>
       </div>
     </div>
